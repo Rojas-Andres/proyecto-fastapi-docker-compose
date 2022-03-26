396642b5af9ac57bd7a4c3ef9f7c61fdac322b93
Rojas-Andres <anferoja@bancolombia.com.co>
2022-03-26 16:39:08 -0500

    limpiando main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 from blog.routers import jsonPlaceHolder,user,album,post
+
 app = FastAPI()
-# app.include_router(authentication.router)
-# app.include_router(blog.router)
-# app.include_router(user.router)
 app.include_router(jsonPlaceHolder.router)
 app.include_router(user.router)
 app.include_router(album.router)
